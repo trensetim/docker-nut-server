0be14630f42c4c96a2fd2514471dfa194a821df7
Tim Trense <74014803+trensetim@users.noreply.github.com>
2021-11-21 13:42:38 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,26 @@
-#! /bin/sh -e
+#! /bin/bash -e
 
+echo "container start initiated"
+echo "resetting /etc/shutdownflag"
+echo "0" > /etc/shutdownflag
+
+stop() {
+  echo "container stop initiated"
+  echo "stopping upsmon"
+  /usr/sbin/upsmon -c stop
+  echo "stopping upsd"
+  /usr/sbin/upsd -u root -c stop
+  echo "stopping upsdrvctl"
+  /usr/sbin/upsdrvctl -u root stop
+  echo "stop completed"
+}
+echo "registering signal trap"
+trap 'stop' SIGTERM
+trap 'stop' SIGINT
+
+echo "starting upsdrvctl"
 /usr/sbin/upsdrvctl -u root start
-/usr/sbin/upsd -u root
+echo "starting upsd"
+/usr/sbin/upsd -u root &
+echo "starting upsmon"
 /usr/sbin/upsmon -D
